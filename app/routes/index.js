05b6c8273dd9d70e31eb9667bb4705638a5ebf68
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2018-03-19 15:09:29 -0300

    Prevent user from navigating back to some screens

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { Router, Scene } from 'react-native-router-flux';
+import { Router, Scene, ActionConst } from 'react-native-router-flux';
 
+import styles from './styles';
 import Main from './Main/Main';
 import Login from './Login/Login';
 
 const Routes = () => (
-    <Router navigationBarStyle={{ backgroundColor: '#115E54' }} titleStyle={{ color: '#fff' }}>
+    <Router navigationBarStyle={styles.navBar} titleStyle={styles.navBarTitle}>
         <Scene key="root">
-            <Scene key='login' component={Login} title="Login" hideNavBar />
-            <Scene key='main' component={Main} title="Main" hideNavBar />
+            <Scene
+                key='login'
+                component={Login}
+                title="Login"
+                hideNavBar
+                type={ActionConst.RESET}
+            />
+            <Scene
+                key='main'
+                component={Main}
+                title="Main"
+                hideNavBar
+                type={ActionConst.RESET}
+            />
         </Scene>
     </Router>
 );
