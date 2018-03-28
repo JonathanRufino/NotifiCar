66069de78a3e033700032ae91083817fbcbcbd04
Luis Henrique <luishng123@gmail.com>
2018-03-28 10:00:50 -0300

    Update account to generate a new node for vehicles and remove firebase config on gitignore

--- app/redux/actions/AccountActions.js
+++ app/redux/actions/AccountActions.js
@@ -1,25 +1,40 @@
 import firebaseApp from '../../services/firebase';
 
-import * as Types from './types';
+import {
+    WRITE_VEHICLE,
+    ADD_VEHICLE_SUCCESS,
+    ADD_VEHICLE_ERROR,
+    REMOVE_VEHICLE,
+    SHOW_DIALOG,
+    UPDATE_VEHICLE_ERROR,
+    FETCH_USER_VEHICLES,
+    SAVING_VEHICLE,
+    FETCH_VEHICLES_IS_LOADING,
+} from './types';
 
 export const writeVehicle = (vehicle) => ({
-    type: Types.WRITE_VEHICLE,
+    type: WRITE_VEHICLE,
     payload: vehicle
 });
 
 export const addVehicle = (userID, vehicle) => (dispatch) => {
     dispatch({
-        type: Types.SAVING_VEHICLE,
+        type: SAVING_VEHICLE,
     });
 
     firebaseApp.database().ref(`/users/${userID}/vehicles`)
         .child(vehicle.toUpperCase())
         .set(true)
+        .then(() => {
+            firebaseApp.database().ref(`/vehicles/${vehicle.toUpperCase()}/`)
+            .child(userID)
+            .set(true);
+        })
         .then(() => dispatch({
-            type: Types.ADD_VEHICLE_SUCCESS,
+            type: ADD_VEHICLE_SUCCESS,
         }))
         .catch(error => dispatch({
-            type: Types.ADD_VEHICLE_ERROR,
+            type: ADD_VEHICLE_ERROR,
             payload: error
         }));
 };
@@ -28,32 +43,37 @@ export const removeVehicle = (userID, vehicle) => dispatch => {
     firebaseApp.database().ref(`/users/${userID}/vehicles`)
         .child(vehicle)
         .remove()
+        .then(() => {
+            firebaseApp.database().ref(`/vehicles/${vehicle.toUpperCase()}/`)
+            .child(userID)
+            .remove();
+        })
         .then(() => dispatch({
-            type: Types.REMOVE_VEHICLE
+            type: REMOVE_VEHICLE
         }))
         .catch(error => dispatch({
-            type: Types.ADD_VEHICLE_ERROR,
+            type: ADD_VEHICLE_ERROR,
             payload: error
         }));
 };
 
 export const showDialog = (dialogIsVisible) => ({
-    type: Types.SHOW_DIALOG,
+    type: SHOW_DIALOG,
     payload: dialogIsVisible
 });
 
 export const updateVehicleError = (error) => ({
-    type: Types.UPDATE_VEHICLE_ERROR,
+    type: UPDATE_VEHICLE_ERROR,
     payload: error
 });
 
 export const fetchUserVehicles = userID => dispatch => {
-    dispatch({ type: Types.FETCH_VEHICLES_IS_LOADING });
+    dispatch({ type: FETCH_VEHICLES_IS_LOADING });
 
     firebaseApp.database().ref(`/users/${userID}/vehicles/`)
         .on('value', snapshot => {
             dispatch({
-                type: Types.FETCH_USER_VEHICLES,
+                type: FETCH_USER_VEHICLES,
                 payload: snapshot.val()
             });
         });
