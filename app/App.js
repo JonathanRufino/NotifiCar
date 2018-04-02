8f08b9bad3006914fb755e344541281a957a8945
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2018-04-02 08:03:26 -0300

    Prevent app from crashing if it's open

--- app/App.js
+++ app/App.js
@@ -38,36 +38,40 @@ export default App;
 // these callback will be triggered even when app is killed
 function registerKilledListener() {
     FCM.on(FCMEvent.Notification, notif => {
-        AsyncStorage.setItem('lastNotification', JSON.stringify(notif));
-
-        FCM.presentLocalNotification({
-            title: Platform.OS === 'ios' ? notif.aps.alert.title : notif.fcm.title,
-            body: Platform.OS === 'ios' ? notif.aps.alert.body : notif.fcm.body,
-            sound: 'default',
-            priority: 'high',
-            vibrate: 300,
-            wake_screen: true,
-            show_in_foreground: true,
-            click_action: 'com.jldevs.notificar', // for ios
-        });
-
-        if (notif.opened_from_tray) {
-            setTimeout(() => {
-                if (notif._actionIdentifier === 'reply') {
-                    if (AppState.currentState !== 'background') {
-                        // Alert.alert(`User replied ${JSON.stringify(notif._userText)}`);
-                        Alert.alert('User replied');
-                    } else {
-                        AsyncStorage.setItem('lastMessage', JSON.stringify(notif._userText));
+        try {
+            AsyncStorage.setItem('lastNotification', JSON.stringify(notif));
+    
+            FCM.presentLocalNotification({
+                title: Platform.OS === 'ios' ? notif.aps.alert.title : notif.fcm.title,
+                body: Platform.OS === 'ios' ? notif.aps.alert.body : notif.fcm.body,
+                sound: 'default',
+                priority: 'high',
+                vibrate: 300,
+                wake_screen: true,
+                show_in_foreground: true,
+                click_action: 'com.jldevs.notificar', // for ios
+            });
+    
+            if (notif.opened_from_tray) {
+                setTimeout(() => {
+                    if (notif._actionIdentifier === 'reply') {
+                        if (AppState.currentState !== 'background') {
+                            // Alert.alert(`User replied ${JSON.stringify(notif._userText)}`);
+                            Alert.alert('User replied');
+                        } else {
+                            AsyncStorage.setItem('lastMessage', JSON.stringify(notif._userText));
+                        }
                     }
-                }
-                if (notif._actionIdentifier === 'view') {
-                    Alert.alert('User clicked View in App');
-                }
-                if (notif._actionIdentifier === 'dismiss') {
-                    Alert.alert('User clicked Dismiss');
-                }
-            }, 1000);
+                    if (notif._actionIdentifier === 'view') {
+                        Alert.alert('User clicked View in App');
+                    }
+                    if (notif._actionIdentifier === 'dismiss') {
+                        Alert.alert('User clicked Dismiss');
+                    }
+                }, 1000);
+            }
+        } catch (err) {
+            console.log(err);
         }
     });
 }
@@ -75,50 +79,54 @@ function registerKilledListener() {
 // these callback will be triggered only when app is foreground or background
 function registerAppListener() {
     FCM.on(FCMEvent.Notification, notif => {
-        if (Platform.OS === 'ios'
-                && notif._notificationType === NotificationType.WillPresent
-                && !notif.local_notification) {
-            // this notification is only to decide if you want to 
-            // show the notification when user if in foreground.
-            // usually you can ignore it. just decide to show or not.
-            notif.finish(WillPresentNotificationResult.All);
-            return;
-        }
-  
-        if (notif.opened_from_tray) {
-            if (notif.targetScreen === 'detail') {
+        try {
+            if (Platform.OS === 'ios'
+                    && notif._notificationType === NotificationType.WillPresent
+                    && !notif.local_notification) {
+                // this notification is only to decide if you want to 
+                // show the notification when user if in foreground.
+                // usually you can ignore it. just decide to show or not.
+                notif.finish(WillPresentNotificationResult.All);
+                return;
+            }
+      
+            if (notif.opened_from_tray) {
+                if (notif.targetScreen === 'detail') {
+                    setTimeout(() => {
+                        Actions.login();
+                    }, 500);
+                }
                 setTimeout(() => {
+                    // Alert.alert(`User tapped notification\n${JSON.stringify(notif)}`);
                     Actions.login();
                 }, 500);
             }
-            setTimeout(() => {
-                // Alert.alert(`User tapped notification\n${JSON.stringify(notif)}`);
-                Actions.login();
-            }, 500);
-        }
-  
-        if (Platform.OS === 'ios') {
-            //optional
-            //iOS requires developers
-            //to call completionHandler to end notification process. If you do not call it your background remote notifications could be throttled, to read more about it see the above documentation link.
-            //This library handles 
-            // it for you automatically with default behavior (for remote notification, finish with NoData; for WillPresent, finish depend on "show_in_foreground"). However if you want to return different result, follow the following code to override
-            //notif._notificationType is available for iOS platfrom
-            switch (notif._notificationType) {
-                case NotificationType.Remote:
-                    notif.finish(RemoteNotificationResult.NewData); //other types available: RemoteNotificationResult.NewData, RemoteNotificationResult.ResultFailed
-                    break;
-                case NotificationType.NotificationResponse:
-                    notif.finish();
-                    break;
-                case NotificationType.WillPresent:
-                    notif.finish(WillPresentNotificationResult.All); //other types available: WillPresentNotificationResult.None
-                    // this type of notificaiton will be called only when you are in foreground.
-                    // if it is a remote notification, don't do any app logic here. Another notification callback will be triggered with type NotificationType.Remote
-                    break;
-                default:
-                    break;
+      
+            if (Platform.OS === 'ios') {
+                //optional
+                //iOS requires developers
+                //to call completionHandler to end notification process. If you do not call it your background remote notifications could be throttled, to read more about it see the above documentation link.
+                //This library handles 
+                // it for you automatically with default behavior (for remote notification, finish with NoData; for WillPresent, finish depend on "show_in_foreground"). However if you want to return different result, follow the following code to override
+                //notif._notificationType is available for iOS platfrom
+                switch (notif._notificationType) {
+                    case NotificationType.Remote:
+                        notif.finish(RemoteNotificationResult.NewData); //other types available: RemoteNotificationResult.NewData, RemoteNotificationResult.ResultFailed
+                        break;
+                    case NotificationType.NotificationResponse:
+                        notif.finish();
+                        break;
+                    case NotificationType.WillPresent:
+                        notif.finish(WillPresentNotificationResult.All); //other types available: WillPresentNotificationResult.None
+                        // this type of notificaiton will be called only when you are in foreground.
+                        // if it is a remote notification, don't do any app logic here. Another notification callback will be triggered with type NotificationType.Remote
+                        break;
+                    default:
+                        break;
+                }
             }
+        } catch (err) {
+            console.log(err);
         }
     });
 }
