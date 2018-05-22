27ceee5b61f60c8eac14d272c1a68953136eaa22
Luis Henrique <luishng123@gmail.com>
2018-05-22 15:53:35 -0300

    Add react image picker in project on Android

--- android/app/src/main/java/com/jldevs/notificar/MainApplication.java
+++ android/app/src/main/java/com/jldevs/notificar/MainApplication.java
@@ -4,6 +4,11 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.evollu.react.fcm.FIRMessagingPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.evollu.react.fcm.FIRMessagingPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
@@ -34,7 +39,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.asList(
         new MainReactPackage(),
-        new SplashScreenReactPackage(),
+            new SplashScreenReactPackage(),
+            new LinearGradientPackage(),
+            new ImagePickerPackage(),
         new FIRMessagingPackage(),
         new FBSDKPackage(mCallbackManager)
       );
